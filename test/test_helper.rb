ab5836cf9f9b34585fcfe7abe8e6f1dbc1a147b9
formindev <formindeve@gmail.com>
2019-12-13 11:08:01 +0400

    Added admin panel

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,3 +16,18 @@ class ActiveSupport::TestCase
 
   include FactoryBot::Syntax::Methods
 end
+
+module SignInHelper
+  def sign_in_as(admin)
+    post session_path, params: {
+      session: {
+        password: admin.password,
+        email: admin.email
+      }
+    }
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
+end
