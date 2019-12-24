fa4b62f407a838efd68b06a5bd590aefddca503b
formindev <formindeve@gmail.com>
2019-12-24 12:40:31 +0400

    Added partial for edit, new form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
