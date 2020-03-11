a3bb56f02435c671aa1ecfb830d3dbd329779d9c
formindev <formindeve@gmail.com>
2020-03-11 20:11:23 +0400

    Added default url option

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,10 @@ import WebpackerReact from "webpacker-react";
 import TaskBoard from "./components/TaskBoard";
 import Routes from "../routes";
 
+Routes.configure({
+  default_url_options: { format: "json" }
+});
+Routes.config();
+
 window.Routes = Routes;
 WebpackerReact.setup({ TaskBoard });
